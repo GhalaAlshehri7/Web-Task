a20db331b2b17006c0684ec8f66e0261e592259a
Ghala <galashrobh@gmail.com>
2026-07-18 22:35:24 +0300

    Update add.php

--- add.php
+++ add.php
@@ -1,1 +1,22 @@
+<?php
+include "db.php";
+
+if(isset($_POST['name']) && isset($_POST['age'])){
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $age = (int)$_POST['age'];
+
+    $sql = "INSERT INTO users (name, age, status)
+            VALUES ('$name', '$age', 0)";
+
+    if(mysqli_query($conn, $sql)){
+        echo "success";
+    }else{
+        echo mysqli_error($conn);
+    }
+
+}else{
+    echo "No data received";
+}
+?>
 
