1c20f97edcca41e9711c756feb2d6067aabed1c5
Ghala <galashrobh@gmail.com>
2026-07-18 22:35:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+document.getElementById("userForm").addEventListener("submit", function(e){
 
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+
+    fetch("add.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: `name=${encodeURIComponent(name)}&age=${encodeURIComponent(age)}`
+    })
+    .then(response => response.text())
+    .then(data => {
+
+        if(data.trim() === "success"){
+            alert("Saved Successfully");
+            location.reload();
+        }else{
+            alert("Error: " + data);
+        }
+
+    })
+    .catch(error => {
+        alert("Error: " + error);
+    });
+
+});
+
+function toggleStatus(id){
+
+    fetch("toggle.php",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/x-www-form-urlencoded"
+        },
+        body:`id=${id}`
+    })
+
+    .then(response=>response.text())
+
+    .then(data=>{
+
+        location.reload();
+
+    });
+
+}
