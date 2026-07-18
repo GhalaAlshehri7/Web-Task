dbf324be9e81533fa9e5b66b3ce6292bc5b828d5
Ghala <galashrobh@gmail.com>
2026-07-18 22:35:53 +0300

    Update toggle.php

--- toggle.php
+++ toggle.php
@@ -1,1 +1,19 @@
+<?php
+include "db.php";
 
+if(isset($_POST['id'])){
+
+    $id = (int)$_POST['id'];
+
+    $sql = "UPDATE users
+            SET status = IF(status=0,1,0)
+            WHERE id=$id";
+
+    if(mysqli_query($conn,$sql)){
+        echo "success";
+    }else{
+        echo "error";
+    }
+
+}
+?>
