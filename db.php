e5f7c7208699a83eed6649a38e0c0876dea77d3d
Ghala <galashrobh@gmail.com>
2026-07-18 22:34:09 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,1 +1,14 @@
+<?php
 
+$host = "sql104.infinityfree.com";
+$username = "if0_42442058";
+$password = "YOUR_DATABASE_PASSWORD";
+$database = "if0_42442058_webtask";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
